d49d781cf2ec0f1ce1d993baee4fb698142e3b84
mds <17163988+mds1@users.noreply.github.com>
2018-10-15 20:10:33 -0400

    Fixed typo in Oyente command

--- sweep.sh
+++ sweep.sh
@@ -8,6 +8,7 @@
 #      ./sweep.sh path/to/Contract.sol
 # ==============================================================================
 
+
 # Setup ------------------------------------------------------------------------
 # Get and start docker images
 docker-compose up
@@ -20,6 +21,7 @@ if [ ! -d outputs ]; then
   mkdir ./outputs
 fi
 
+
 # Run tools --------------------------------------------------------------------
 # Run Mythril, pipe output to mythril.out
 docker container run -v $(pwd)/contracts:/contracts mythril/myth -x \
@@ -30,10 +32,12 @@ docker container run -v $(pwd)/contracts:/contracts trailofbits/slither \
   /${contractName} > outputs/slither.out 2>&1
 
 # Run Oyente, pipe output to oyente.out
+oyenteCommand="cd oyente && python oyente.py -s contracts/${contractName} > outputs/oyente.out 2>&1"
+
 docker container run -v $(pwd)/contracts:/oyente/oyente/contracts \
   -v $(pwd)/outputs:/oyente/oyente/outputs luongnguyen/oyente \
-  /bin/bash -c \
-  "cd oyente &&  python oyente.py -s contracts/Reentrancy.sol > outputs/oyente.out 2>&1"
+  /bin/bash -c ${oyenteCommand}
+
 
 # Tear down --------------------------------------------------------------------
 # Shut down Docker environments
